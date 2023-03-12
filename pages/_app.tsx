02ac98a400145aa3dc5329bdc58ec349dfa72f15
Websidesign <service@websidesign.nl>
2023-03-12 15:05:26 +0100

    GA fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,31 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as ga from "../lib/ga";
+
 import type { AppProps } from "next/app";
-import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-602BMHPS6K"
-      ></Script>
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-602BMHPS6K', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Component {...pageProps} />
       <Analytics />
     </>
