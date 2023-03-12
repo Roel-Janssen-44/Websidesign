39d257c78f8c1f03151e502103ca720d20823a36
Websidesign <service@websidesign.nl>
2023-03-12 16:00:03 +0100

    mina

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,28 @@
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
+
 import { Analytics } from "@vercel/analytics/react";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 import "../styles/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      /* invoke analytics function only for production */
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
-      {console.log("process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ")}
-      {console.log(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID)}
-      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
       <Analytics />
     </>
