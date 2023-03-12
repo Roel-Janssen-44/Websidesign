0478b505a6141a0fa3bf5bd4d57521c2d5160576
Websidesign <service@websidesign.nl>
2023-03-12 15:35:44 +0100

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,13 @@
-import Script from "next/script";
-import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 import "../styles/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    // @ts-ignore */
-    window.dataLayer = window.dataLayer || [];
-    // @ts-ignore */
-    window.gtag = function () {
-      // @ts-ignore */
-      dataLayer.push(arguments);
-    };
-    // @ts-ignore */
-    window.gtag("js", new Date());
-    // @ts-ignore */
-    window.gtag("config", "<GA-id>");
-  }, []);
   return (
     <>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-602BMHPS6K"
-      />
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-602BMHPS6K`}
-      />
-
-      <Script id="ga-script" strategy="lazyOnload">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-602BMHPS6K', {
-        page_path: window.location.pathname,
-        });
-    `}
-      </Script>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
       <Analytics />
     </>
