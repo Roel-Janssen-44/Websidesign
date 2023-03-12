7aa2478e1fe0a5ec0b1c083ab0d24e71da375fc3
Websidesign <service@websidesign.nl>
2023-03-12 15:30:48 +0100

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,30 @@
 import Script from "next/script";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // @ts-ignore */
+    window.dataLayer = window.dataLayer || [];
+    // @ts-ignore */
+    window.gtag = function () {
+      // @ts-ignore */
+      dataLayer.push(arguments);
+    };
+    // @ts-ignore */
+    window.gtag("js", new Date());
+    // @ts-ignore */
+    window.gtag("config", "<GA-id>");
+  }, []);
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-602BMHPS6K"
+      />
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=G-602BMHPS6K`}
