7caed8fec661a8adbc7f37015e4a4c9a44c95fc3
Websidesign <service@websidesign.nl>
2023-03-12 15:16:45 +0100

    Google analytics fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,27 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as ga from "../lib/ga";
-
+import Script from "next/script";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      ga.pageview(url);
-    };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-602BMHPS6K`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-602BMHPS6K', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,25 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="nl">
-      <Head>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=G-602BMHPS6K`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-602BMHPS6K', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
