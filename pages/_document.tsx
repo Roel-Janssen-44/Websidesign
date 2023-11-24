07b31c5815eba1e89252583eec3104bd0e1ebe90
Roel-Janssen-44 <roeljanssen2002@gmail.com>
2023-11-24 17:36:21 +0100

    added og image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,18 @@ export default function Document() {
   return (
     <Html lang="nl">
       <Head>
+        <meta
+          property="og:url"
+          content="https://www.websidesign.nl/og-image.png"
+        />
+        <meta
+          property="og:image"
+          content="https://www.websidesign.nl/images/meta-tags.png"
+        />
+
         <meta property="og:type" content="website" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Head>
       <body>
         <Main />
