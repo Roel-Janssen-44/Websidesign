f0d4772e3bfc039a81355c0b07709ac6aa6427e2
Roel-Janssen-44 <roeljanssen2002@gmail.com>
2023-11-24 17:29:04 +0100

    add meta og:type

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="nl">
-      <Head />
+      <Head>
+        <meta property="og:type" content="website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
